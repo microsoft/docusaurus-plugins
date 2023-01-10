1a27a3169acb2d09cbbd0e68d4d002428a0a52fc
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-01-10 14:48:53 +0000

    extract code remark plugin in place

--- packages/docusaurus-remark-plugin-extract-code/src/index.ts
+++ packages/docusaurus-remark-plugin-extract-code/src/index.ts
@@ -1,16 +1,10 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
+/// <refere
 import visit from "unist-util-visit";
 import type { Code, Content, Literal } from "mdast";
 import type { Plugin } from "unified";
 import type { Node, Parent } from "unist";
 import { ensureDirSync, writeFileSync } from "fs-extra";
-import { join } from "path";
+import { join, resolve, basename, dirname } from "path";
 
 type PluginOptions = {
     outputPath?: string;
@@ -24,15 +18,15 @@ const plugin: Plugin<[PluginOptions?]> = (options = {}) => {
 
     let snippet = 0;
     return (root, file) => {
+        const { history } = file;
+        const fpath = history[history.length - 1] || "unknown";
+        const fbase = basename(fpath);
         visit(root, "code", (node: Code) => {
             const { lang, value } = node;
             if (lang && langs.indexOf(lang) > -1) {
-                console.log(file);
-                const fn = join(
-                    outputPath,
-                    `${file.name}.${snippet.toFixed(3)}.${lang}`
-                );
-                writeFileSync(fn, value, { encoding: "utf8" });
+                const fn = join(outputPath, `${fbase}.${snippet}.${lang}`);
+                ensureDirSync(dirname(fn))
+                writeFileSync(fn, value, { encoding: "utf8",  });
                 snippet++;
             }
         });
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,90 +6,96 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Rise4Fun plugins for Docusaurus",
-  tagline: "Rise4Fun plugins for Docusaurus",
-  url: "https://microsoft.github.io",
-  baseUrl: "/docusaurus-plugins-rise4fun",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+    title: "Rise4Fun plugins for Docusaurus",
+    tagline: "Rise4Fun plugins for Docusaurus",
+    url: "https://microsoft.github.io",
+    baseUrl: "/docusaurus-plugins-rise4fun",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "microsoft", // Usually your GitHub org/user name.
-  projectName: "docusaurus-plugins-rise4fun", // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "microsoft", // Usually your GitHub org/user name.
+    projectName: "docusaurus-plugins-rise4fun", // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          remarkPlugins: [
-            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
-            [require("docusaurus-remark-plugin-cli-output"), { sync: true }],
-          ],
-        },
-        blog: false,
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "Rise4Fun plugins",
-        logo: {
-          alt: "Rise4fun Logo",
-          src: "img/logo.svg",
-        },
-        items: [],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "GitHub",
-                href: "https://github.com/microsoft/docusaurus-plugins-rise4fun",
-              },
-            ],
-          },
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    remarkPlugins: [
+                        [
+                            require("@docusaurus/remark-plugin-npm2yarn"),
+                            { sync: true },
+                        ],
+                        [
+                            require("docusaurus-remark-plugin-extract-code"),
+                            { outputPath: "snippets", langs: ["js"] },
+                        ],
+                    ],
+                },
+                blog: false,
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            }),
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Microsoft. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-
-  plugins: [
-    [
-      "docusaurus-plugin-application-insights",
-      {
-        instrumentationKey: "foobar",
-      },
     ],
-  ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: "Rise4Fun plugins",
+                logo: {
+                    alt: "Rise4fun Logo",
+                    src: "img/logo.svg",
+                },
+                items: [],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Docs",
+                    },
+                    {
+                        title: "More",
+                        items: [
+                            {
+                                label: "GitHub",
+                                href: "https://github.com/microsoft/docusaurus-plugins-rise4fun",
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Microsoft. Built with Docusaurus.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
+
+    plugins: [
+        [
+            "docusaurus-plugin-application-insights",
+            {
+                instrumentationKey: "foobar",
+            },
+        ],
+    ],
 };
 
 module.exports = config;
