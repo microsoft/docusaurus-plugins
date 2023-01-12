daf0110396bae456865e318167dbb9f46d240fba
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-01-12 17:52:54 +0000

    fixing sell point

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -1,69 +1,71 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
+    title: string;
+    Svg: React.ComponentType<React.ComponentProps<"svg">>;
+    description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Leverage the goodness of Docusaurus, designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Battery included',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-      Out of the box, accessibility, analytics, math, tool execution. Focus on the content instead.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+    {
+        title: "Easy to Use",
+        Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+        description: (
+            <>
+                Leverage the goodness of Docusaurus, designed from the ground up
+                to be easily installed and used to get your website up and
+                running quickly.
+            </>
+        ),
+    },
+    {
+        title: "Battery included",
+        Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+        description: (
+            <>
+                Out of the box, accessibility, analytics, math, tool execution.
+                Focus on the content instead.
+            </>
+        ),
+    },
+    {
+        title: "Built for GitHub Actions and Pages",
+        Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+        description: (
+            <>
+                Designed for open source tools on GitHub, tested on GitHub
+                Actinos and hosted on Github Pages.
+            </>
+        ),
+    },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ title, Svg, description }: FeatureItem) {
+    return (
+        <div className={clsx("col col--4")}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} role="img" />
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
