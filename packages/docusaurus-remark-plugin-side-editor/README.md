6e316d5f267b195b3e2423172fe00ffc63252265
pelikhan <jhalleux@microsoft.com>
2023-01-21 16:03:21 -0800

    remark side editor

---
+++ packages/docusaurus-remark-plugin-side-editor/README.md
@@ -0,0 +1,7 @@
+# `@rise4fun/docusaurus-remark-plugin-side-editor`
+
+Injects `Edit` button after code section that open snippet in side editor.
+
+## Usage
+
+See [docusaurus-remark-plugin-side-editor documentation](https://microsoft.github.io/docusaurus-plugins-rise4fun/docs/plugins/docusaurus-remark-plugin-side-editor).
---
+++ packages/docusaurus-remark-plugin-side-editor/src/__tests__/index.test.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import vfile from "to-vfile";
+import mdx from "remark-mdx";
+import remark from "remark";
+import plugin from "../index";
+
+const processFixture = async (name: string, options: any) => {
+    const filePath = path.join(__dirname, "__fixtures__", `${name}.md`);
+    const file = await vfile.read(filePath);
+    const result = await remark().use(mdx).use(plugin, options).process(file);
+
+    return result.toString();
+};
+
+const options = {
+    languages: {
+        ts: "typescript",
+    },
+};
+
+describe("code-tabs plugin", () => {
+    it("works on edit file", async () => {
+        const result = await processFixture("edit", options);
+        expect(result).toMatchSnapshot();
+    });
+});
