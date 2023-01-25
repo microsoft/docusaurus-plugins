0c5e6526e633a077b660061d687fbc4304fc7a3b
pelikhan <jhalleux@microsoft.com>
2023-01-25 05:06:57 -0800

    removing msagl deps

--- website/src/theme/Dot/index.tsx
+++ website/src/theme/Dot/index.tsx
@@ -1,30 +1,10 @@
-import { parseDot } from "@msagl/parser";
-import { LayoutOptions, Renderer } from "@msagl/renderer";
-import React, { useEffect, useRef } from "react";
-
-import styles from "./styles.module.css";
+import React, { lazy, Suspense } from "react";
 
 export default function Dot(props: {
     lang: string;
-    meta: LayoutOptions;
+    meta: object;
     value: string;
 }) {
-    const { lang, meta, value } = props;
-    const containerRef = useRef();
-    const renderer = useRef<Renderer>();
-
-    useEffect(() => {
-        const container = containerRef.current;
-        if (!renderer.current) renderer.current = new Renderer(container);
-        const graph = parseDot(value);
-        renderer.current.setGraph(graph, meta);
-    }, [value, JSON.stringify(meta)]);
-
-    return (
-        <div
-            data-lang={lang}
-            className={styles.dotContainer}
-            ref={containerRef}
-        ></div>
-    );
+    const { value } = props;
+    return <pre>{value}</pre>;
 }
