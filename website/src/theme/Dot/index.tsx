66316f2b3b8a71972bde67b2d6fe343d86d9b350
pelikhan <jhalleux@microsoft.com>
2023-01-25 04:45:53 -0800

    feat: support for code element mapping

--- website/src/theme/Dot/index.tsx
+++ website/src/theme/Dot/index.tsx
@@ -1,8 +1,30 @@
+import { parseDot } from "@msagl/parser";
+import { LayoutOptions, Renderer } from "@msagl/renderer";
+import React, { useEffect, useRef } from "react";
+
+import styles from "./styles.module.css";
+
 export default function Dot(props: {
     lang: string;
-    meta: object;
+    meta: LayoutOptions;
     value: string;
 }) {
     const { lang, meta, value } = props;
-    return value;
+    const containerRef = useRef();
+    const renderer = useRef<Renderer>();
+
+    useEffect(() => {
+        const container = containerRef.current;
+        if (!renderer.current) renderer.current = new Renderer(container);
+        const graph = parseDot(value);
+        renderer.current.setGraph(graph, meta);
+    }, [value, JSON.stringify(meta)]);
+
+    return (
+        <div
+            data-lang={lang}
+            className={styles.dotContainer}
+            ref={containerRef}
+        ></div>
+    );
 }
