fb784e7eb5367e0db99457da2360321d0b2eb5f6
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-12-02 16:58:27 +0000

    upgrade to sha256

--- packages/docusaurus-remark-plugin-compile-code/src/hash.ts
+++ packages/docusaurus-remark-plugin-compile-code/src/hash.ts
@@ -3,7 +3,7 @@ import { LangOptions } from './types';
 import { readFileSync } from 'fs-extra';
 
 export default function hashCode(source: string, meta: string, options: LangOptions) {
-  const hash = createHash('sha1');
+  const hash = createHash('sha256');
   hash.update(source).update(meta).update(JSON.stringify(options));
   options.hashFiles?.forEach((file) => {
     hash.update(readFileSync(file));
